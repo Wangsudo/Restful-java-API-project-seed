0eaaf4d7b2a7291bc76d374cc377754d4a783b14
王顺 <walker_ws@yeah.net>
2018-03-18 15:59:37 +0800

    Update RestfulApiProjectSeedApplication.java

--- src/main/java/com/walker/restfulapiprojectseed/RestfulApiProjectSeedApplication.java
+++ src/main/java/com/walker/restfulapiprojectseed/RestfulApiProjectSeedApplication.java
@@ -3,7 +3,7 @@ package com.walker.restfulapiprojectseed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"comgit "})
+@SpringBootApplication(scanBasePackages = {"com"})
 public class RestfulApiProjectSeedApplication {
 
 	public static void main(String[] args) {
