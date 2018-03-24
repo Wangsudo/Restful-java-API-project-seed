bbdaa3ceb49421cd4f1b35401d60353334ded414
shunW <971997088@qq.com>
2018-03-24 15:32:57 +0800

    /*add redis test*/

--- src/test/java/com/walker/restfulapiprojectseed/RestfulApiProjectSeedApplicationTests.java
+++ src/test/java/com/walker/restfulapiprojectseed/RestfulApiProjectSeedApplicationTests.java
@@ -1,16 +1,26 @@
 package com.walker.restfulapiprojectseed;
 
+import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestfulApiProjectSeedApplicationTests {
 
-	@Test
-	public void contextLoads() {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    //测试redis配置是否成功
+    @Test
+	public void test() {
+        stringRedisTemplate.opsForValue().set("name","jichun");
+        Assert.assertEquals("jichun",stringRedisTemplate.opsForValue().get("name"));
 	}
 
 }
