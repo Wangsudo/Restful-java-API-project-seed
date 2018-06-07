712e840a7d3d528ecac715e8c56c6055de38600c
ws <ws@viroyal.cn>
2018-06-07 11:09:54 +0800

    wangshun push

--- consumer/src/main/java/com/walker/rediscomsumer/Consumer.java
+++ consumer/src/main/java/com/walker/rediscomsumer/Consumer.java
@@ -1,14 +1,20 @@
 package com.walker.rediscomsumer;
 
+import com.walker.rediscomsumer.service.RedisQueueListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by wangshun on 2018/3/24
  **/
 @SpringBootApplication
 public class Consumer {
-    private static final Logger Logger = LoggerFactory.getLogger(Consumer.class);
+    private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
     
     @Autowired
     private RedisQueueListener queueListener;
@@ -20,8 +26,8 @@ public class Consumer {
     @Bean
     CommandLineRunner startServer(){
         return args->{
-            logger.info("start);
-            queueListener.startUp();
-        }
+            logger.info("start");
+            queueListener.startup();
+        };
     }
 }
